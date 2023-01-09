1439d2454a9c033de5ae8e86fa69a41c5eed59c5
Crowdbotics <team@crowdbotics.com>
2023-01-09 20:15:59 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "epx-mobile-app-38233",
+  url: "https://epx-mobile-app-38233.botics.co",
+  api: "https://epx-mobile-app-38233.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
